6a56234081969a049db7340e1f8efb37d93999c0
Pavel Trhlik <trhlikpa@gmail.com>
2016-07-11 16:34:17 +0200

    -intial mongodb commit //TODO -limit to scified field during mongodb find -add celery task time limit

--- thugworker/thugtask.py
+++ thugworker/thugtask.py
@@ -1,4 +1,4 @@
-from celery import Celery
+from celery import Celery, task
 import json
 import logging
 import hashlib
@@ -7,12 +7,16 @@ import io
 from ThugAPI import *
 from Plugins.ThugPlugins import *
 from datetime import datetime
+from pymongo import MongoClient
 
 with io.open('../config.json', encoding='utf8') as f:
     config = json.load(f)
 
 celery = Celery('thugtasks', broker=config['CELERY_BROKER_URL'])
 celery.conf.update(config)
+
+db_client = MongoClient(config['MONGODB_URL'])
+db = db_client.thug_database
 
 __log__ = logging.getLogger('Thug')
 __log__.setLevel(logging.WARN)
@@ -21,39 +25,63 @@ __logpath__ = '/opt/thug/logs/'
 
 
 class ThugScript(ThugAPI):
-    def __init__(self, url, useragent):
-        self._url = url
-        self._useragent = useragent
+    def __init__(self, cfg):
+        if not cfg or 'url' not in cfg:
+            raise ValueError('URL not found')
+
+        self._cfg = cfg
         ThugAPI.__init__(self, None, __cfgpath__)
 
     def analyze(self):
         self.set_file_logging()
         self.set_json_logging()
-
-        self.log_init(self._url)
-        self.set_useragent(self._useragent)
         self.set_log_quiet()
 
-        url_md5 = hashlib.md5(self._url).hexdigest()
+        if len(self._cfg.get('useragent', '')) > 0:
+            self.set_useragent(self._cfg['useragent'])
+
+        if len(self._cfg.get('java', '')) > 0:
+            self.set_javaplugin(self._cfg['java'])
+        else:
+            self.disable_javaplugin()
+
+        if len(self._cfg.get('shockwave', '')) > 0:
+            self.set_shockwave_flash(self._cfg['shockwave'])
+        else:
+            self.disable_shockwave_flash()
+
+        if len(self._cfg.get('adobepdf', '')) > 0:
+            self.set_acropdf_pdf(self._cfg['adobepdf'])
+        else:
+            self.disable_acropdf()
+
+        if len(self._cfg.get('proxy', '')) > 0:
+            self.set_proxy(self._cfg['proxy'])
+
+        self.log_init(self._cfg['url'])
+
+        url_md5 = hashlib.md5(self._cfg['url']).hexdigest()
         time = datetime.now().strftime('%Y%m%d%H%M%S%f')
         log_path = os.path.join(__logpath__, url_md5, time)
 
         self.set_log_dir(log_path)
         ThugPlugins(PRE_ANALYSIS_PLUGINS, self)()
-        self.run_remote(self._url)
+        self.run_remote(self._cfg['url'])
         ThugPlugins(POST_ANALYSIS_PLUGINS, self)()
 
         self.log_event()
         return log_path
 
 
-@celery.task
-def check_url(url, useragent):
-    thug = ThugScript(url, useragent)
+@celery.task(bind='true')
+def analyze_url(self, data):
+    thug = ThugScript(data)
     log_path = thug.analyze()
-
     json_path = os.path.join(log_path, 'analysis/json/analysis.json')
 
     with io.open(json_path) as result:
         json_data = json.load(result)
-        return json_data
+        json_data['_id'] = self.request.id
+        db.tasks.insert(json_data)
+        db_client.close()
+        return db_client.database_names()
--- webclient/app.py
+++ webclient/app.py
@@ -1,73 +1,19 @@
-import json
 import io
-from flask import Flask, request, render_template, jsonify
-from thugworker.thugtask import check_url
+import json
+from flask import Flask
 
+# load configuration
 with io.open('../config.json', encoding='utf8') as f:
     config = json.load(f)
 
 app = Flask(__name__)
 app.config.update(config)
 
-tasks = list()
+# Import task module
+from webclient.tasks.controllers import tasks_blueprint
 
-
-@app.route('/')
-def index():
-    return render_template('index.html', tasks=tasks)
-
-
-@app.route('/thugtask', methods=['POST'])
-def thugtask():
-    data = {}
-    json_data = json.loads(request.data)
-
-    for entry in json_data:
-        data.setdefault(entry['name'], []).append(entry['value'])
-
-    if len(data['url']) <= 0:
-        return jsonify({}), 400
-
-    if len(data['useragent']) <= 0:
-        data['useragent'].append('winxpie60')
-
-    for agent in data['useragent']:
-        task = check_url.apply_async(args=[data['url'][0], agent])
-        tasks.append({
-            'task_id': task.id,
-            'url': data['url'][0],
-            'useragent': agent
-        })
-
-    return jsonify({}), 202
-
-
-@app.route('/status/<task_id>')
-def taskstatus(task_id):
-    task = check_url.AsyncResult(task_id)
-
-    response = {
-        'state': task.state,
-        'result': task.info
-    }
-
-    return jsonify(response)
-
-
-@app.route("/get_tasks")
-def get_tasks():
-    output = {
-        'iTotalRecords': len(tasks),
-        'iTotalDisplayRecords': len(tasks),
-        'sEcho': str(int(request.values['sEcho'])),
-        'aaData': []
-    }
-
-    for entry in tasks:
-        output['aaData'].append(entry)
-
-    return jsonify(output)
-
+# Register blueprints
+app.register_blueprint(tasks_blueprint)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
