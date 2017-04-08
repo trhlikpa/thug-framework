cdc988dbd4aa54b1749b25f71091273832da2b00
trhlikpa <trhlikpa@gmail.com>
2017-04-08 14:47:33 +0200

    updated docker files

--- scheduler/config.py
+++
@@ -1,2 +0,0 @@
-BROKER_URL = 'redis://redis:6379'
-MONGODB_URL = 'mongodb://db:27017'
--- webclient/config.py
+++
@@ -1,6 +0,0 @@
-DEBUG = True
-ADMINS = ['email@example.com']
-SECRET_KEY = 'secret-key'
-THREADS_PER_PAGE = 2
-
-MONGODB_URL = 'mongodb://db:27017'
